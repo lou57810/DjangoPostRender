85a3cf1a804556abb3fb30f6601f580315308649
lou57810 <bernard.oswald@free.fr>
2025-02-04 13:45:31 +0100

    first commit

---
+++ market/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=120)
+    ticker = models.CharField(max_length=20, unique=True, db_index=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
